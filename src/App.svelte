a62ad08b50e279a0ccbb8a6532739da916f7b176
Juuso Takala <juuso.takala86@gmail.com>
2021-04-13 00:59:20 +0300

    sass to dev packages, style reset in index.html,

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,26 @@
 <script>
+  import Header from './components/header.svelte'
+  let first_name = 'Seppo'
 </script>
 
+<!-- Imported component header from src/components/header.svelte-->
+<Header text={'This is a Header!'} />
 <div class="main">
   <h1>svelte-sass-template</h1>
-  <p>SASS is working!</p>
+  <p class="red">SASS is working!</p>
+  <p>Your name is {first_name}</p>
 </div>
 
+<!-- Remember type="text/scss" in style if u are using scss -->
 <style type="text/scss">
-  @import './sass/global.scss';
   $color: red;
+
   .main {
-    p {
+    background-color: blue;
+    h1 {
+      color: yellow;
+    }
+    .red {
       background: $color;
     }
   }
---
+++ src/components/header.svelte
@@ -0,0 +1,9 @@
+<script>
+  export let text
+</script>
+
+<header>{text}</header>
+
+<style type="text/scss">
+  @import '../sass/header.scss';
+</style>
